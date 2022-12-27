75acf5de80cdbaff51bbf003fea4b516eb4835cb
Gyunald <97107396+Gyunald@users.noreply.github.com>
2022-12-27 13:25:25 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,28 +4,54 @@ import datetime
 import requests
 from streamlit_lottie import st_lottie
 
-st.header('Club')
+st.header('IMI Critical Engineering Korea Club')
+import pandas as pd
+import streamlit as st
+import datetime
+import requests
+from streamlit_lottie import st_lottie
+from streamlit_extras.switch_page_button import switch_page
+
+
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="😎",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
 c1, c2, c3 = st.columns([1,1,1])
 
 with c1:
-    st.write('[![this is an image link](https://cdn.pixabay.com/photo/2021/03/02/19/26/snowshoes-6063630_960_720.jpg)](https://www.streamlit.io/)')
-    st.success('배드민턴')
+    st.image('https://cdn.pixabay.com/photo/2021/03/02/19/26/snowshoes-6063630_960_720.jpg',use_column_width=True)
+    if st.button('배드민턴'):
+        switch_page('배드민턴')
+    
 with c2:
     st.image('https://cdn.pixabay.com/photo/2019/01/21/13/58/table-tenis-3946115_960_720.jpg',use_column_width=True)
-    st.success('[탁구](%s)' % 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=탁구')
+    if st.button('탁구'):
+        switch_page('탁구')
 
 with c3:
     st.image('https://cdn.pixabay.com/photo/2018/03/08/20/36/ball-3209809_960_720.jpg',use_column_width=True)
-    st.success('[풋살](%s)' % 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=풋살')
+    if st.button('풋살'):
+        switch_page('풋살')
 
 
 c4, c5, c6 = st.columns([1,1,1])
 
 with c4:
     st.image('https://cdn.pixabay.com/photo/2015/11/20/08/17/meat-1052571_960_720.jpg',use_column_width=True)
-    st.success('[파티](%s)' % 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=파티')
+    if st.button('파티'):
+        switch_page('파티')
 
 with c5:
     st.image('https://cdn.pixabay.com/photo/2016/04/23/20/21/smart-1348189_960_720.jpg',use_column_width=True)
-    st.success('[카풀](%s)' % 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=카풀')
+    if st.button('카풀'):
+        switch_page('카풀')
 
